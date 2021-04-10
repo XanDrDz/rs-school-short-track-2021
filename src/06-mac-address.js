5506443fd4c28282933c389bc3e4bb8cfa1170f3
Dzyubinskiy Aleksandr <timik_333@mail.ru>
2021-04-10 19:33:14 +0500

    feat: add solution file names

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const valid = /^([A-F0-9]{2}-){5}[A-F0-9]{2}$/g;
+  return valid.test(n);
 }
 
 module.exports = isMAC48Address;
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,26 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr = [names];
+  const res = [];
+  let count = 1;
+  const count2 = 1;
+  for (let i = 0; i < names.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (names[i] === res[j]) {
+        const after = (`${names[i]}(${count})`);
+        res.push(after);
+        count++;
+        break;
+      } else if (`${res[j]}(${count2})` === `${names[i]}`) {
+        res.push(`${names[i]}(${count2})`);
+      } else {
+        res.push(names[i]);
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = renameFiles;
