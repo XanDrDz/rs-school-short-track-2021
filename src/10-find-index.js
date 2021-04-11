3f78521f893e0d0676fa0276c7a146898710ef77
Dzyubinskiy Aleksandr <timik_333@mail.ru>
2021-04-11 13:17:57 +0500

    feat: add find-index solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,22 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let low = 0;
+  let high = (array.length - 1);
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const positionMid = array[mid];
+    if (positionMid === value) {
+      return mid;
+    }
+    if (positionMid > value) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
 }
 
 module.exports = findIndex;
--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const arraySplit = email.split('@');
+  return arraySplit[arraySplit.length - 1];
 }
 
 module.exports = getEmailDomain;
