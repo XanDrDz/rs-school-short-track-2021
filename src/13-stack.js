5799c1b6e9a3b713192032bb4ff0b199a227446b
Dzyubinskiy Aleksandr <timik_333@mail.ru>
2021-04-12 20:58:23 +0500

    feat: add stack solution

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,23 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.stack = [];
+    this.up = 0;
+  }
+
+  push(element) {
+    const el = element;
+    this.up = el;
+    return this.stack.push(el);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.stack.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.stack[this.stack.length - 1];
   }
 }
 
